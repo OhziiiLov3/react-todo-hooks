9f4ed771b9d950103175819fc574f194eaac6b26
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-07 13:52:13 -0700

    added todo and todo form components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Todo from './components/Todo';
+
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list">
+      {todos.map((todo,index)=>(
+        <Todo
+          key={index}
+          index={index}
+          todo={todo}
+          />
+      ))}
+    </div>
     </div>
   );
 }
